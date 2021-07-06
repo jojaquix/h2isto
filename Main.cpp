0d1e2a68451343a36b749dc4ce401662afc825c8
Jhon James Quintero <jhon.quintero@alertlogic.com>
2021-07-06 13:21:24 -0500

    basic project set up including unit tests support

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by jhon on 6/07/21.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hola mundo";
+    return 0;
+}
+
---
+++ utests/UTests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by jhon on 6/07/21.
+//
+
+#include <gtest/gtest.h>
+
+TEST(basicTests, myTest)
+{
+    EXPECT_EQ(1000, 1000);
+}
